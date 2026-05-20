7f02541c3e4c6cefebab6707d10da853a86e8b3d
michanoku <michael.werker@gmail.com>
2026-05-20 04:38:14 +0000

    feat: Add user related queries

---
+++ db/queries.js
@@ -0,0 +1,22 @@
+const pool = require("./pool");
+
+const createUser = async ({ firstName, lastName, email, hash }) => {
+    const { rows } = await pool.query(`
+        INSERT INTO users (first_name, last_name, email, hash) VALUES ($1, $2, $3, $4) RETURNING *;
+        `, [firstName, lastName, email, hash]);
+    return rows[0];
+}
+
+const findUserByEmail = async (email) => {
+    const { rows } = await pool.query(`
+        SELECT * FROM users WHERE email = $1;
+        `, [email]);
+    return rows[0];
+}
+
+const findUserById = async (userId) => {
+    const { rows } = await pool.query(`
+        SELECT * FROM users WHERE user_id = $1;
+        `, [userId]);
+    return rows[0];
+}
