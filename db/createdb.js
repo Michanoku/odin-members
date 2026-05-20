ceaa8a7b2a17667b2c406f7b383aaa183967dff6
michanoku <michael.werker@gmail.com>
2026-05-20 04:24:30 +0000

    feat: Add table schemas

--- db/createdb.js
+++ db/createdb.js
@@ -2,7 +2,22 @@ require("dotenv").config();
 const { Client } = require("pg");
 
 const SQL = `
-TODO
+CREATE TABLE IF NOT EXISTS users (
+  user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(64) NOT NULL,
+  last_name VARCHAR(64) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  member BOOLEAN NOT NULL DEFAULT FALSE,
+  admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  message_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255) NOT NULL,
+  message TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  user_id INTEGER references users(user_id)
+);
 `;
 
 async function main() {
