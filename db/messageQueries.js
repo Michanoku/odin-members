7f8e0c311e56fd8f7b442b71f2d4891011fbfa6b
michanoku <michael.werker@gmail.com>
2026-05-22 06:27:17 +0000

    feat: Add message queries

---
+++ db/messageQueries.js
@@ -0,0 +1,35 @@
+const pool = require("./pool");
+
+const createMessage = async ({ title, message, userId }) => {
+    await pool.query(`
+        INSERT INTO messages (title, message, user_id) VALUES ($1, $2, $3);
+        `, [title, message, userId]);
+}
+
+const getAllMessages = async () => {
+    const { rows } = await pool.query(`
+        SELECT 
+        messages.message_id, 
+        messages.title, 
+        messages.text, 
+        messages.created_at, 
+        users.first_name, 
+        users.last_name 
+        FROM messages 
+        JOIN users 
+        ON messages.user_id = users.user_id;
+        `);
+    return rows;
+}
+
+const deleteMessage = async (messageId) => {
+    await pool.query(`
+        DELETE FROM messages WHERE message_id = $1;
+        `, [messageId]);
+}
+
+module.exports = {
+    createMessage,
+    getAllMessages,
+    deleteMessage,
+}
