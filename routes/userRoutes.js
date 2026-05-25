3c1d3fcd317127d59a1a824005615f89a70ae3dd
michanoku <michael.werker@gmail.com>
2026-05-25 06:56:05 +0000

    feat: add join routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const authMiddleware = require("../passport/authMiddleware");
 
 router.get("/register", userController.getRegister);
 router.post("/register", userController.postRegister);
 router.get("/login", userController.getLogin);
 router.post("/login", userController.postLogin);
 router.get("/logout", userController.getLogout);
+router.get("/join", authMiddleware.isAuth, userController.getJoin);
+router.post("/join", authMiddleware.isAuth, userController.postJoin);
 
 module.exports = router;
